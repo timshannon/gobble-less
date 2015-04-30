7c1c929b0a37f41ca3c616e10a2ec912ac154117
Tim Shannon <shannon.timothy@gmail.com>
2015-04-30 21:02:35 +0000

    Completed?  Not much else to do.

--- index.js
+++ index.js
@@ -2,35 +2,30 @@ module.exports = function less(inputdir, outputdir, options, callback) {
     var path = require('path'),
         sander = require('sander');
 
-	//TODO: all of this - copied from gobble-sass
-
     if (!options.src || !options.dest) {
         throw new Error('gobble-less requires `options.src` and `options.dest` to be set');
     }
-
-    options.file = path.join(inputdir, options.src);
+    options.filename = path.join(inputdir, options.src);
 
     if (options.sourceMap === undefined) {
         // by default, generate sourcemaps and include comments
-        options.sourceMap = options.dest + '.map';
-        options.sourceMapContents = options.sourceMapContents !== false;
+        options.sourceMap = {};
     }
+    var input = sander.readFileSync(options.filename).toString();
+    require('less').render(input, options)
+        .then(function(result) {
+                var promises = [
+                    sander.writeFile(outputdir, options.dest, result.css)
+                ];
 
-    options.error = callback;
+                if (result.map) {
+                    promises.push(sander.writeFile(outputdir, options.dest + '.map', result.map));
+                }
 
-    options.success = function(result) {
-        var promises = [
-            sander.writeFile(outputdir, options.dest, result.css)
-        ];
+                sander.Promise.all(promises).then(function() {
+                    callback();
+                }, callback);
 
-        if (result.map) {
-            promises.push(sander.writeFile(outputdir, options.dest + '.map', result.map));
-        }
-
-        sander.Promise.all(promises).then(function() {
-            callback();
-        }, callback);
-    };
-
-    require('less').render(options);
+            },
+            callback);
 };
